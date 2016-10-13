bcf54168f8a9646b1fcfc253503536864f1ef11d
teemoleary <727630072@qq.com>
2016-10-13 15:20:02 +0800

    two

--- app/src/main/java/cn/ucai/fulicenter/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/MainActivity.java
@@ -2,6 +2,7 @@ package cn.ucai.fulicenter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(MainActivity.this, "Hello Gitthub", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "Hello GitHub", Toast.LENGTH_SHORT).show();
+        Log.i("main", "Hello GitHub");
     }
 }
