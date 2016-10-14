34c8230192c9d88f894a1f0d0158025e8a822bd0
teemoleary <727630072@qq.com>
2016-10-14 19:46:56 +0800

    在MainActivity中实例化RadioButton的id，写出onCheckedChange方法，实现按钮互斥效果

--- app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
@@ -3,17 +3,37 @@ package cn.ucai.fulicenter.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.utils.L;
 
 public class MainActivity extends AppCompatActivity {
-
+    RadioButton mLayoutNewGood,mLayoutCart,mLayoutBoutique,mLayoutCategory, mLayoutPersonal;
+    TextView mtvGoodsNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         L.i("MainActivity onCreate");
+        initView();
+    }
+
+    private void initView() {
+        mLayoutCart = (RadioButton) findViewById(R.id.layout_cart);
+        mLayoutBoutique = (RadioButton) findViewById(R.id.layout_boutique);
+        mLayoutCategory = (RadioButton) findViewById(R.id.layout_category);
+        mLayoutPersonal = (RadioButton) findViewById(R.id.personal);
+        mLayoutNewGood = (RadioButton) findViewById(R.id.layout_new_good);
+        mtvGoodsNumber = (TextView) findViewById(R.id.tvGoodsNumber);
+    }
+
+    public void onCheckedChange(View view) {
+        switch (view.getId()) {
+
+        }
     }
 }
