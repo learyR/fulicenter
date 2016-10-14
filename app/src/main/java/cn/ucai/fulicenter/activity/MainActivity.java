483ed7d3d9e1fbb139cb0d920bf4188d5cf1f5cf
teemoleary <727630072@qq.com>
2016-10-14 15:54:44 +0800

    添加MFGT工具类到SplashActivity中使用，添加Log输出工具L并在main中测试

--- app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
@@ -1,9 +1,12 @@
-package cn.ucai.fulicenter;
+package cn.ucai.fulicenter.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
+
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.utils.L;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        L.i("MainActivity onCreate");
     }
 }
