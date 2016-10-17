4f21c3c613851c3961b7a418efbb71667c73b27b
teemoleary <727630072@qq.com>
2016-10-17 11:45:38 +0800

    添加newGoods的基本框架

---
+++ app/src/main/java/cn/ucai/fulicenter/adapter/NewGoodsAdapter.java
@@ -0,0 +1,115 @@
+package cn.ucai.fulicenter.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.NewGoodsBean;
+import cn.ucai.fulicenter.utils.I;
+
+/**
+ * Created by Administrator on 2016/10/17.
+ */
+public class NewGoodsAdapter extends RecyclerView.Adapter {
+    List<NewGoodsBean> newGoodsList = new ArrayList<>();
+    Context context;
+
+    RecyclerView parent;
+    String textFooter;
+    boolean isMore;
+
+    public boolean isMore() {
+        return isMore;
+    }
+
+    public void setMore(boolean more) {
+        isMore = more;
+    }
+
+    public void setTextFooter(String textFooter) {
+        this.textFooter = textFooter;
+        notifyDataSetChanged();
+    }
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        this.parent = (RecyclerView) parent;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = null;
+        RecyclerView.ViewHolder holder = null;
+        switch (viewType) {
+            case I.TYPE_FOOTER:
+                view = inflater.inflate(R.layout.item_footer, parent, false);
+                holder = new FooterViewHolder(view);
+                break;
+            case I.TYPE_ITEM:
+                view = inflater.inflate(R.layout.item_newgoods, parent, false);
+                holder = new NewGoodsViewHolder(view);
+                break;
+
+        }
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (position == getItemCount() - 1) {
+            FooterViewHolder footerViewHolder = (FooterViewHolder) holder;
+            footerViewHolder.tvFooter.setText(textFooter);
+            return;
+        }
+        NewGoodsViewHolder newGoodsViewHolder = (NewGoodsViewHolder) holder;
+        NewGoodsBean newGoods = newGoodsList.get(position);
+        newGoodsViewHolder.tvGoodsName.setText(newGoods.getGoodsName());
+        newGoodsViewHolder.tvGoodsPrice.setText(newGoods.getCurrencyPrice());
+    }
+
+    @Override
+    public int getItemCount() {
+        return newGoodsList==null?0:newGoodsList.size()+1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == getItemCount() - 1) {
+            return I.TYPE_FOOTER;
+        } else {
+            return I.TYPE_ITEM;
+        }
+    }
+
+    static class NewGoodsViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.ivNewGoods)
+        ImageView ivNewGoods;
+        @Bind(R.id.tvGoodsName)
+        TextView tvGoodsName;
+        @Bind(R.id.tvGoodsPrice)
+        TextView tvGoodsPrice;
+
+
+        public NewGoodsViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+    }
+
+    static class FooterViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.tvFooter)
+        TextView tvFooter;
+        FooterViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
