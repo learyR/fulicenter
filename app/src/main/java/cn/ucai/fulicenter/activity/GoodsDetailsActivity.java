11fb2ad13f722e74a80c869eecba1e479b849fb1
teemoleary <727630072@qq.com>
2016-10-26 15:58:12 +0800

    商品详情中对是否收藏进行逻辑判断，改变收藏图标的状态

--- app/src/main/java/cn/ucai/fulicenter/activity/GoodsDetailsActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/GoodsDetailsActivity.java
@@ -2,15 +2,19 @@ package cn.ucai.fulicenter.activity;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.ucai.fulicenter.FuLiCenterApplication;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.bean.AlbumsBean;
 import cn.ucai.fulicenter.bean.GoodsDetailsBean;
+import cn.ucai.fulicenter.bean.MessageBean;
+import cn.ucai.fulicenter.bean.User;
 import cn.ucai.fulicenter.net.NetDao;
 import cn.ucai.fulicenter.net.OkHttpUtils;
 import cn.ucai.fulicenter.utils.CommonUtils;
@@ -36,12 +40,16 @@ public class GoodsDetailsActivity extends BaseActivity {
     int goodsId;
     GoodsDetailsActivity mContext;
 
+    boolean isCollect = false;
+    @Bind(R.id.ivGoodsDetailCollect)
+    ImageView ivGoodsDetailCollect;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_goods_details);
         ButterKnife.bind(this);
         goodsId = getIntent().getIntExtra(I.GoodsDetails.KEY_GOODS_ID, 0);
-        L.i("details"+goodsId);
+        L.i("details" + goodsId);
         if (goodsId == 0) {
             finish();
         }
@@ -51,6 +59,7 @@ public class GoodsDetailsActivity extends BaseActivity {
         setListener();
 */
     }
+
     @Override
     protected void setListener() {
 
@@ -68,6 +77,7 @@ public class GoodsDetailsActivity extends BaseActivity {
                     finish();
                 }
             }
+
             @Override
             public void onError(String error) {
                 finish();
@@ -81,7 +91,7 @@ public class GoodsDetailsActivity extends BaseActivity {
         tvEnglishName.setText(details.getGoodsEnglishName());
         tvGoodsName.setText(details.getGoodsName());
         tvGoodsPrice.setText(details.getCurrencyPrice());
-        salv.startPlayLoop(indicator,getAlbumImgUrl(details),getAlbumImgCount(details));
+        salv.startPlayLoop(indicator, getAlbumImgUrl(details), getAlbumImgCount(details));
         wbGoodsBrief.loadDataWithBaseURL(null, details.getGoodsBrief(), I.TEXT_HTML, I.UTF_8, null);
     }
 
@@ -97,7 +107,7 @@ public class GoodsDetailsActivity extends BaseActivity {
         if (details.getProperties() != null && details.getProperties().length > 0) {
             AlbumsBean[] albums = details.getProperties()[0].getAlbums();
             urls = new String[albums.length];
-            for (int i=0;i<albums.length;i++) {
+            for (int i = 0; i < albums.length; i++) {
                 urls[i] = albums[i].getImgUrl();
             }
         }
@@ -108,11 +118,51 @@ public class GoodsDetailsActivity extends BaseActivity {
     protected void initView() {
         mContext = this;
     }
+
     @OnClick(R.id.ivBack)
-    public  void onBackClick(){
+    public void onBackClick() {
         MFGT.finish(this);
     }
-//    public void onBackPressed(){
+
+    //    public void onBackPressed(){
 //        MFGT.finish(this);
 //    }
+    private void isCollect() {
+        User user = FuLiCenterApplication.getUser();
+        if (user != null) {
+            NetDao.isCollect(mContext, user.getMuserName(), goodsId, new OkHttpUtils.OnCompleteListener<MessageBean>() {
+                @Override
+                public void onSuccess(MessageBean result) {
+                    if (result != null && result.isSuccess()) {
+                        isCollect = true;
+                        updateGoodsCollectsStatus();
+                    } else {
+                        isCollect = false;
+                        updateGoodsCollectsStatus();
+                    }
+                }
+
+                @Override
+                public void onError(String error) {
+                    isCollect = false;
+                    updateGoodsCollectsStatus();
+                }
+            });
+        }
+
+    }
+
+    private void updateGoodsCollectsStatus() {
+        if (isCollect) {
+            ivGoodsDetailCollect.setImageResource(R.mipmap.bg_collect_out);
+        } else {
+            ivGoodsDetailCollect.setImageResource(R.mipmap.bg_collect_in);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isCollect();
+    }
 }
